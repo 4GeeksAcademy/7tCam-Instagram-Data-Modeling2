c6ec8a8b92211333761fbe21bad8aac50e1a4709
luisgomez7t <162279891+7tCam@users.noreply.github.com>
2024-08-27 17:46:55 +0000

    avance

--- src/models.py
+++ src/models.py
@@ -1,32 +1,109 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-
+# para pract.
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# tags = Table('tags',
+#     Column('tag_id', Integer, ForeignKey('tag.id'), primary_key=True),
+#     Column('page_id', Integer, ForeignKey('page.id'), primary_key=True)
+# )
+
+# class Page(Model):
+#     id = Column(Integer, primary_key=True)
+#     tags = relationship('Tag', secondary=tags, lazy='subquery',
+#         backref=backref('pages', lazy=True))
+
+# class Tag(Model):
+    # id = Column(Integer, primary_key=True)
+# claves foraneas
+#name variable | declarando una tabla
+
+followers = Table('followers',
+    Base.metadata,
+    Column('user_from_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('user_to_id', Integer, ForeignKey('user.id'), primary_key=True)
+)
+class Follower(Base):
+    __tablename__ = 'folllowers'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+#commet 
+# class Address(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # email = db.Column(db.String(120), nullable=False)
+    # person_id = db.Column(db.Integer, db.ForeignKey('person.id'),
+    #     nullable=False)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    commment_text = Column(String)
+    #            entero       | de donde viene el id foraneo |
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post = relationship('Comment', backref='post', lazy=True)
+    post = relationship('Media', backref='post', lazy=True)
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    typ = Column(Enum(),nullable=False)
+    url = Column(String)
+    user_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+
+# class Person(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(50), nullable=False)
+#     addresses = db.relationship('Address', backref='person', lazy=True)
+
+# class Address(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), nullable=False)
+#     person_id = db.Column(db.Integer, db.ForeignKey('person.id'),
+#         nullable=False)
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    # relationship se coloca en la tabla donde esta el id principal(funcion que relaciona tablas)
+    # name coherente      |nameClase donde esata la clave foranea| nombre de la tabla actual
+    comment = relationship('Commet', backref='user', lazy=True)
+    post = relationship('Post', backref='user', lazy=True)
+
 
     def to_dict(self):
         return {}
+
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
